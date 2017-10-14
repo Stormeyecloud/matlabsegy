3b32311b80824c52f650508fe60c4d7ca078f2e5
Stormeyecloud <stormeye126@126.com>
2017-10-14 19:01:24 +0800

    commit added

--- demo.m
+++ demo.m
@@ -1,3 +1,8 @@
+%% Step1:Preparation
+% Download Crews Matlab toolbox at https://www.crewes.org/ResearchLinks/FreeSoftware/crewes.zip
+% Unzip Crews zip file and add them to matlab path
+%% Step2:Locate and source Segy file
+%  source the Segy file
 tic
 % s=SegyFile('pstm_cg_150.segy','r',[],[],[],[],[],[],[],[],1);
 % []= Fig progressbar; 1=text progressbar ; 0=no progressbar;
@@ -5,9 +10,12 @@ tic
 path=strcat(segyPath,segyName);
 s=SegyFile(path,'r');
 toc
-%%
-% choose the directory to save your Records Data. Suggest saving to working
-% directory
+%% Step3:Generate records data
+% This will take a while if the segy file is huge. Once the record file is
+% generated and we don't need to go through this procedure.
+
+% Choose the directory to save your Records Data. Suggest saving to working
+% directory.
 tic
 [recordsName, recordsPath] = uiputfile('records.dat');
 if recordsName == 0
@@ -15,40 +23,40 @@ if recordsName == 0
 end
 
 fileID = fopen(strcat(recordsPath,recordsName),'w');
-if fileID == -1 
+if fileID == -1 % Check the file existence
     errordlg('File does not exist, check your file path or file permission')
     return
 end
+
 fwrite(fileID,s.Trc.read(1:300),'double');
 fclose(fileID);
 msgbox(['[' segyName ']', ' converted to:',' ' ,'[' recordsName,']' , ', path: ' ,' ' ,recordsPath,recordsName])
 toc
-%%
-tic
-%m = memmapfile('records.dat');
-% m=memmapfile('records.dat','Format',{'double' [xLines inLines dataPoints] 'x'});
+%% Step3:Link the records data to Matlab workspace
 
+% input basic survy info
 sample = s.FileInfo.SamplesPerTrace;
 maxTrace = s.Trc.FileInfo.TracesInFile;
+% Only when dealing with 3D survy
 xlineNum = 369;
 inlineNum = 661;
 
+% segy 2D read format:
+%SurvyFormat = double([sample maxTrace]);
 % segy 3D read format:
 %SurvyFormat = double([sample xlineNum inlineNum]);
-%m = memmapfile('filepath\filename','Format',{'double', SurvyFormat, 'x'})
-
-% segy 2D read format:
-%m = memmapfile('filepath\filename','Format',{'double', SurvyFormat, 'x'})
 
 SurvyFormat = double([sample xlineNum inlineNum]);
+
+% Map records data into workspace
 m = memmapfile('records.dat','Format',{'double', SurvyFormat, 'x'});
 
-% get seismic as m.Data.x(sampleRange,xlineRange,inlineRange)
+% Retrive seismic as m.Data.x(sampleRange,xlineRange,inlineRange)
 load lmkRWB
 % set xline=150 profile
 figure;imagesc(squeeze(m.Data.x(:,150,:)));colormap(lmkrwb);
 % set t=1500 time slice
 figure;imagesc(squeeze(m.Data.x(1500,:,:)));colormap(lmkrwb);
-toc
+
 
 
